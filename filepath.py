ad88ef005dd1ae4cb4b95f2ce07b3ddc5fb33b3a
Ariel Perez <arielperezch@gmail.com>
2017-01-08 01:35:09 -0300

    Python 3 compatibility

--- filepath.py
+++ filepath.py
@@ -4,6 +4,21 @@
 import os
 import ntpath
 
+try:
+    unicode = unicode
+except NameError:
+    # 'unicode' is undefined, must be Python 3
+    str = str
+    unicode = str
+    bytes = bytes
+    basestring = (str, bytes)
+else:
+    # 'unicode' exists, must be Python 2
+    str = str
+    unicode = unicode
+    bytes = str
+    basestring = basestring
+
 
 def remove(paths):
     """Remove all files in the given array."""
